176ab2a737d0fecc893c08e0937ff60f99decd4d
unknown <nsteiner@.united.domain>
2011-02-24 13:21:06 +0100

    removed old files

--- defaults/preferences/HttpFox.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.httpfox.StartAtBrowserStart", false);
-pref("extensions.httpfox.AlwaysOpenDetached", false);
-pref("extensions.httpfox.ShowHttpFoxHelperRequests", false);
-pref("extensions.httpfox.ColorRequests", true);
-pref("extensions.httpfox.ShowDebugTab", false);
-pref("extensions.httpfox.ForceCaching", true);
